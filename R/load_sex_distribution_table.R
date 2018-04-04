401fd80f2b26a97e7b66bcb5aea4cd300082897d
Romain Feron <romain.feron@evobio.eu>
2018-04-04 15:49:30 +0200

    Minor fixes

--- R/load_sex_distribution_table.R
+++ R/load_sex_distribution_table.R
@@ -9,11 +9,11 @@
 #' @examples
 #' data = load_sex_distribution_table(input_file_path)
 
-load_sex_distribution_table = function(input_file_path) {
+load_sex_distribution_table <- function(input_file_path) {
 
-    data = suppressMessages(readr::read_delim(input_file_path, "\t", escape_double = FALSE, col_names = TRUE, trim_ws = TRUE))
-    n_males = max(data$Males)
-    n_females = max(data$Females)
+    data <- suppressMessages(readr::read_delim(input_file_path, "\t", escape_double = FALSE, col_names = TRUE, trim_ws = TRUE))
+    n_males <- max(data$Males)
+    n_females <- max(data$Females)
 
-    return(list(data=data, n_males=n_males, n_females=n_females))
+    return(list(data = data, n_males = n_males, n_females = n_females))
 }

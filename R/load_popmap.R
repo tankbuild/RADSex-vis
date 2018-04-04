f943bd8b1ce2752d292ef646e67a59fb6f989961
Romain Feron <romain.feron@evobio.eu>
2018-04-04 15:58:52 +0200

    Implemented load popmap function

---
+++ R/load_popmap.R
@@ -0,0 +1,18 @@
+#' @title Popmap loader
+#'
+#' @description Load a population map.
+#'
+#' @param input_file_path Path to a population map.
+#'
+#' @return A vector with individuals as names and sex as values.
+#'
+#' @examples
+#' popmap = load_popmap(input_file_path)
+
+load_popmap <- function(input_file_path) {
+
+    data <- suppressMessages(readr::read_delim(input_file_path, "\t", escape_double = FALSE, col_names = FALSE, trim_ws = TRUE))
+    popmap <- data$X2
+    names(popmap) <- data$X1
+    return(popmap)
+}

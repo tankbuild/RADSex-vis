1bf61bd747d3302f14f62b00426cfbaee6bc78b5
Romain Feron <romain.feron@evobio.eu>
2018-04-06 15:13:52 +0200

    Implemented load names file function

---
+++ R/load_contig_names_file.R
@@ -0,0 +1,19 @@
+#' @title Load a contig names file
+#'
+#' @description Loads a table of contig names.
+#'
+#' @param input_file_path Path to a contig names file
+#'
+#' @return A vector with contigs as names and contig names as values.
+#'
+#' @examples
+#' lengths = load_names_file(input_file_path)
+
+load_names_file <- function(input_file_path, group.unplaced=TRUE) {
+
+    raw_data <- suppressMessages(read_delim(input_file_path, "\t", escape_double = FALSE, col_names = FALSE,  trim_ws = TRUE))
+    data = raw_data$X2
+    names(data) = raw_data$X1
+
+    return(data)
+}

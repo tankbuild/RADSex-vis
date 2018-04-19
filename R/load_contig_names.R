b76143fcec1008c14f09fe46d07856bf053b07a2
Romain Feron <romain.feron@evobio.eu>
2018-04-19 15:53:40 +0200

    Small doc fix

--- R/load_contig_names.R
+++ R/load_contig_names.R
@@ -4,16 +4,24 @@
 #'
 #' @param input_file_path Path to a contig names file.
 #'
-#' @return A vector with contigs as names and contig names as values.
+#' @return A vector with contigs as names and contig names as values, or NULL if there is no input file
 #'
 #' @examples
 #' lengths <- load_contig_names("contig_names.tsv")
 
 load_contig_names <- function(input_file_path) {
 
-    raw_data <- suppressMessages(readr::read_delim(input_file_path, "\t", escape_double = FALSE, col_names = FALSE,  trim_ws = TRUE))
-    data <- raw_data$X2
-    names(data) <- raw_data$X1
+    if (!is.null(input_file_path)) {
 
-    return(data)
+        raw_data <- suppressMessages(readr::read_delim(input_file_path, "\t", escape_double = FALSE, col_names = FALSE,  trim_ws = TRUE))
+        data <- raw_data$X2
+        names(data) <- raw_data$X1
+        return(data)
+
+    } else {
+
+        return(NULL)
+
+    }
+
 }
